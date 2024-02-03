7a36ca68ea02b3eedf94ce36b2f7d2249a13e21e
Hashirkhan24 <khn24202@gmail.com>
2024-02-03 12:51:41 +0530

    Create test api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import connectDb from './database/db.js';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 
 dotenv.config()
 const app = express();
 connectDb()
+
+app.use('/api/user', userRoutes)
+
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
