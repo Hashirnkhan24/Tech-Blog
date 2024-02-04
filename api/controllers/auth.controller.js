6fa94ba1770b936e806e11362ed8588cce1a3125
Hashirkhan24 <khn24202@gmail.com>
2024-02-04 11:07:36 +0530

    Create Sig In api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import { signupValidator } from "../types.js";
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 const signup = async(req, res, next) => {
     const { username, email, password } = req.body;
@@ -13,7 +15,7 @@ const signup = async(req, res, next) => {
                 })
             }
             // Creating User
-            const hashedPassword = bcryptjs.hashSync(password, 10)
+            const hashedPassword = bcryptjs.hashSync(password, 10) //Hashing the password to protect it even inside db
 
             const newUser = await User({
                 username: username,
@@ -38,4 +40,32 @@ const signup = async(req, res, next) => {
         }
 }
 
-export {signup}
+const signin = async(req, res, next) => {
+    const { email, password } = req.body;
+    if(!email || !password || email === "" || password === "") {
+        return res.status(411).json({ message: "Email and Password required"})
+    }  
+
+    try {
+        const validUser = await User.findOne({ email })
+            if(!validUser) {
+                return res.status(404).json({ message: "User Not Found"})
+            }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+            if(!validPassword) {
+                return res.status(411).json({ message: "Invalid Credentials"})
+            }
+        const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET_KEY)
+        const {password: pass, ...rest} = validUser._doc; //To seperate password as we dont want to send it to the client side
+
+        res.status(200)
+        .cookie('ACCESS_TOKEN', token, {
+            httpOnly: true
+        })
+        .json(rest)
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error"})
+    }
+}
+export {signup, signin}
