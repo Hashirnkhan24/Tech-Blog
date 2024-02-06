c47cacbe521a9abcaa078ad5406bc9ce57a9272e
Hashirkhan24 <khn24202@gmail.com>
2024-02-06 19:58:28 +0530

    Complete upload post image functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,24 +1,32 @@
 import Post from '../models/post.model.js'
 export const create = async(req, res, next) => {
 
-    if(!req.user.isAdmin) {
-        return res.status(401).json({
-            message : "You must be an admin"
+    try {
+        if(!req.user.isAdmin) {
+            return res.status(401).json({
+                message : "You must be an admin"
+            })
+        }
+        if(!req.body.title || !req.body.content) {
+            return res.status(401).json({
+                message : "Please provide all the necessary fields"
+            })
+        }
+        const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+        const newPost = new Post({
+            ...req.body, 
+            slug, 
+            userId: req.user.id
         })
+    
+        const savedPost = await newPost.save()
+        res.status(201).json(savedPost)
+    } catch (error) {
+        if (error.code === 11000 && error.keyPattern && error.keyValue) {
+            return res.status(400).json({
+                message: `Post with title '${req.body.title}' already exists`
+            });
+        }
+        next(error);
     }
-    if(!req.body.title || !req.body.content) {
-        return res.status(401).json({
-            message : "Please provide all the necessary fields"
-        })
-    }
-
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
-    const newPost = new Post({
-        ...req.body, 
-        slug, 
-        userId: req.user.id
-    })
-
-    const savedPost = await newPost.save()
-    res.status(201).json(savedPost)
 }
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,54 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react'
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react'
 import { DashSidebar } from '../components'
 import { Editor } from '@tinymce/tinymce-react'
+import { useState } from 'react'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 function CreatePost() {
+    const [file, setFile] = useState(null)
+    const [imageUploadProgress, setimageUploadProgress] = useState(null);
+    const [imageUploadError, setimageUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+
+    const handleUploadImage = async() => {
+        try {
+            if(!file) {
+                setimageUploadError("Please provide an image")
+                return;
+            }
+            setimageUploadError(null)
+            const storage = getStorage(app)
+            const fileName = new Date().getTime() + '-' + file.name;
+            const storageRef = ref(storage, `images/${fileName}`);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setimageUploadProgress(progress.toFixed(0))
+                },
+                (error) => {
+                    console.log(error)
+                    setimageUploadError("Image upload failed")
+                    setimageUploadProgress(null)
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setimageUploadError(null)
+                        setimageUploadProgress(null)
+                        setFormData({...formData, image: downloadURL})
+                    })
+                }
+            )
+        } catch (error) {
+            setimageUploadError("Error uploading image")
+            setimageUploadProgress(null)
+            console.log(error)
+        }
+    }
     const handleEditorInput = () => {}
 
     return (
@@ -32,9 +78,46 @@ function CreatePost() {
                             <option value="nodejs">Node JS</option>
                         </Select>
                     </div>
-                    <div className='flex p-2 gap-4 items-center justify-between border-4 border-blue-600 border-dotted'>
-                        <FileInput type="file" accept='images/*' style={{borderRadius: '2px', padding:'4px'}}/>
-                        <Button type='button' className='rounded-sm bg-gradient-to-r from-red-700 via-red-600 to-red-700 hover:from-blue-700 hover:via-blue-600 hover:to-blue-700' size='sm'> Upload Image</Button>
+                    <div className='border-4 border-blue-600 border-dotted'>
+                    <div className='flex p-2 gap-4 items-center justify-between'>
+                        <FileInput 
+                            type="file" 
+                            accept='images/*' 
+                            style={{borderRadius: '2px', padding:'4px'}}
+                            onChange={(e) => setFile(e.target.files[0])}
+                        />
+                        <Button 
+                            type='button' 
+                            className='rounded-sm bg-gradient-to-r from-red-700 via-red-600 to-red-700 hover:from-blue-700 hover:via-blue-600 hover:to-blue-700' 
+                            size='sm'
+                            onClick={handleUploadImage}
+                            disabled={imageUploadProgress}
+                        > {
+                            imageUploadProgress
+                            ? (<div className='w-16 h-16'>
+                                <CircularProgressbar 
+                                    value={imageUploadProgress}
+                                    text={`${imageUploadProgress || 0}%`}
+                                />
+                            </div>)
+                            : ("Upload Image")
+                        }
+                        </Button>
+                    </div>
+                    <div>
+                    {
+                            imageUploadError && (
+                            <Alert color="failure" >{imageUploadError}</Alert>
+                        )}
+                        {
+                            formData.image && (
+                                    <img 
+                                        src={formData.image} 
+                                        alt="image"
+                                        className='w-full h-76 object-cover' />
+                            )
+                        }
+                    </div>
                     </div>
                     <Editor 
                         
